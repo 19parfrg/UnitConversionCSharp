245e546ee71d626865d063ef9b4e3e09415b248a
Unknown <soph.bohr22@gmail.com>
2018-06-21 14:33:57 -0500

    fixed zee button

--- Controllers/FirstController.cs
+++ Controllers/FirstController.cs
@@ -19,10 +19,20 @@ namespace UnitConversionCSharp.Controllers
         {
             return View( viewName: "AgeConversion", model: 0.0);
         }
+        public ActionResult TimeConversions()
+        {
+            return View(viewName: "TimeConversions", model: 0.0);
+        }
+
         [HttpPost]
         public ActionResult convertAge(double inputAge)
         {
             return View(viewName: "AgeConversion", model:inputAge*7);
+        }
+        [HttpPost]
+        public ActionResult ConvertTime(double inputShakes)
+        {
+            return View(viewName: "TimeConversions", model: inputShakes * 10);
         }
     }
 }
